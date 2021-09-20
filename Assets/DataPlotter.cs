809eb9087dec09b0ccdd4807e97bbc2f2c3b08dd
Abimbola Popoola <abimbolapopoola57@gmail.com>
2021-09-20 22:13:53 +0100

    first commit

---
+++ Assets/DataPlotter.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlotter : MonoBehaviour {
+
+    // Name of the input file, no extension
+    public string inputfile;
+    
+    // List for holding data from CSV reader
+    private List<Dictionary<string, object>> pointList;
+    
+    // Indices for columns to be assigned
+    public int columnX = 1;
+    public int columnY = 2;
+    public int columnZ = 3;
+    public int columnT = 4;
+    
+    // Full column names
+    public string xName;
+    public string yName;
+    public string zName;
+    public string tName;
+
+    // The prefab for the data points to be instantiated
+    public GameObject PointPrefab;
+
+    // The prefab for the data points that will be instantiated
+    public GameObject PointHolder;
+
+    // Use this for initialization
+    void Start () {
+
+        // Set pointlist to results of function Reader with argument inputfile
+        pointList = CSVReader.Read(inputfile);
+        
+        //Log to console
+        Debug.Log(pointList);
+
+        // Declare list of strings, fill with keys (column names)
+        List<string> columnList = new List<string>(pointList[1].Keys);
+ 
+        // Print number of keys (using .count)
+        Debug.Log("There are " + columnList.Count + " columns in CSV");
+        
+        foreach (string key in columnList)
+        Debug.Log("Column name is " + key);
+
+        // Assign column name from columnList to Name variables
+        xName = columnList[columnX];
+        yName = columnList[columnY];
+        zName = columnList[columnZ];
+        tName = columnList[columnT];
+
+        //Loop through Pointlist
+        for (var i = 0; i < pointList.Count; i++) {
+            // Get value in poinList at ith "row", in "column" Name
+            float x = System.Convert.ToSingle(pointList[i][xName]);
+            float y = System.Convert.ToSingle(pointList[i][yName]);
+            float z = System.Convert.ToSingle(pointList[i][zName]);
+            float t = System.Convert.ToSingle(pointList[i][tName]);
+            
+            // Instantiate as gameobject variable so that it can be manipulated within loop
+            GameObject dataPoint = Instantiate(
+            PointPrefab, 
+            new Vector3(x, y, z), 
+            Quaternion.identity);
+
+            // Make dataPoint child of PointHolder object 
+            dataPoint.transform.parent = PointHolder.transform;
+
+            // Rotate the data to the appropriate orientation
+            PointHolder.transform.Rotate(-90.0f, 180.0f, 0.0f, Space.Self);
+
+            // Assigns original values to dataPointName
+            string dataPointName = 
+            pointList[i][xName] + " "
+            + pointList[i][yName] + " "
+            + pointList[i][zName];
+
+            // Assigns name to the prefab
+            dataPoint.transform.name = dataPointName;
+
+            // RGB color
+            int r = 255;
+            int g = 0;
+            int b = 0;
+
+            switch (t) {
+                case float n when n <= 294.0:
+                    r = 0;
+                    g = 255;
+                    b = 0;
+                    break;
+                case float n when n > 294.0 && n <= 296.0:
+                    r = 204;
+                    g = 204;
+                    b = 0;
+                    break;
+                case float n when n > 296.0:
+                    r = 255;
+                    g = 0;
+                    b = 0;
+                    break;
+                default:
+                    r = 255;
+                    g = 0;
+                    b = 0;
+                    break;
+            }
+
+            // Gets material color and sets it to a new RGBA color we define
+            dataPoint.GetComponent<Renderer>().material.color = 
+            new Color(r,g,b, 1.0f);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# TLT VR
+## Design and Simulation of Air-conditioning System in a Large Auditorium
+
+Tools Used - Unity, GoogleVR, Autodesk Revit
+
+### Overview
+The aim of this project is to:
+1. Create a 3D model of a lecture theatre,
+2. Design an air-conditioning system for the theatre,
+3. Simulate the indoor air distribution, and
+4. Visualise the setup in a virtual environment.
+
+The virtual environment gives users access to experience 3D in a vivid and immersive way.
+
+
+![TLT Front](./pictures/TLTFront.PNG "TLT Front")
+TLT Front
+
+---
+![TLT Back](./pictures/TLTBack.PNG "TLT Back")
+TLT Back
+
+---
+![TLT Plot](./pictures/TLTPlot.PNG "TLT Plot")
+TLT Plot
